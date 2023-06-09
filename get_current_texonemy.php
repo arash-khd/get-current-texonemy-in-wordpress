82e844eb1c4f106ebc9203dc1c0c0c1e34cc13f7
arash-khd <126689940+arash-khd@users.noreply.github.com>
2023-06-09 20:40:16 +0330

    Update get_current_texonemy.php

--- get_current_texonemy.php
+++ get_current_texonemy.php
@@ -2,4 +2,16 @@
 
 //Get taxonomy name for the current post
 
-$countries = get_the_terms( $post_id, 'country' );
+$artist = get_the_terms( $post_id, 'artist' );
+echo $artist[0]->name; 
+
+//get all terms of a custom texonemy
+
+            $terms = get_terms([
+                'taxonomy' => "category",
+                'hide_empty' => false,
+            ]);
+foreach($terms As $term) {?>
+    
+
+<?php }   ?>
